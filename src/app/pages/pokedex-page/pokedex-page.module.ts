c60d198270b2e65e45e4c255e94201be6f14e1ae
Daniel Vilela <dabvilela@gmail.com>
2024-04-03 21:22:23 -0300

    comit inicial

---
+++ src/app/pages/pokedex-page/pokedex-page.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PokedexPageComponent } from './pokedex-page.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+  ],
+  declarations: [
+    PokedexPageComponent,
+
+  ]
+})
+export class PokedexPageModule { }
