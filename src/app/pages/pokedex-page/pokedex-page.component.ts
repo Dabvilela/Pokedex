c9061c76f51820811cf10c6302ba82dc3b712d8c
Daniel Vilela <dabvilela@gmail.com>
2024-04-03 23:15:11 -0300

    add bootstrap e material no projeto

--- src/app/pages/pokedex-page/pokedex-page.component.ts
+++ src/app/pages/pokedex-page/pokedex-page.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { HeaderComponent } from '../../Components/header/header.component';
 
 @Component({
+  standalone: true,
   selector: 'app-pokedex-page',
   templateUrl: './pokedex-page.component.html',
-  styleUrls: ['./pokedex-page.component.scss']
+  styleUrls: ['./pokedex-page.component.scss'],
+  imports:[HeaderComponent]
 })
 export class PokedexPageComponent implements OnInit {
 
