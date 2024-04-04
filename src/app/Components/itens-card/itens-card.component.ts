7b1b4cf75f5ab789ebe0d1a3ebcb8ebbe318c889
Daniel Vilela <dabvilela@gmail.com>
2024-04-03 21:32:14 -0300

    subindo alteração para teste

---
+++ src/app/Components/itens-card/itens-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-itens-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './itens-card.component.html',
+  styleUrl: './itens-card.component.scss'
+})
+export class ItensCardComponent {
+
+}
