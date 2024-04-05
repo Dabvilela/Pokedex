ba39cef42dd6f2f30d22ff57a0dc8706b970c4c4
Daniel Vilela <dabvilela@gmail.com>
2024-04-04 21:10:13 -0300

    add Toolbar and sideNavBar

--- src/app/Components/sidebar/sidebar.component.ts
+++ src/app/Components/sidebar/sidebar.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import {MatListModule} from '@angular/material/list';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MediaMatcher} from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatSidenavModule, MatListModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
+  mobileQuery: MediaQueryList;
 
+  fillerNav = Array.from({length: 5}, (_, i) => `Nav Item ${i + 1}`);
+
+  private _mobileQueryListener: () => void;
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
+
+  shouldRun = true;
 }
+
--- src/app/pages/pokedex-page/pokedex-page.component.ts
+++ src/app/pages/pokedex-page/pokedex-page.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from '../../Components/header/header.component';
+import { SidebarComponent } from '../../Components/sidebar/sidebar.component';
 
 @Component({
   standalone: true,
   selector: 'app-pokedex-page',
   templateUrl: './pokedex-page.component.html',
   styleUrls: ['./pokedex-page.component.scss'],
-  imports:[HeaderComponent]
+  imports:[HeaderComponent, SidebarComponent]
 })
 export class PokedexPageComponent implements OnInit {
 
